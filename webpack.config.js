5f47083b78b7fd501641773cc54ee7733a6a5010
Jonathan Kula <jonathan@jdkula.dev>
2023-06-10 04:35:33 -0700

    Reset and reimplement webpack situation – it was super old before, lots of dead code, wouldn't compile without python2

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,49 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const HandlebarsPlugin = require("handlebars-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/js/main.js",
-    output: {
-        path: path.resolve(__dirname, './docs'),
-        publicPath: "",
-        filename: "scripts/bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                // SCSS
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract(['css', 'resolve-url', 'sass?sourceMap'])
-            }, {
-                // Standard css
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-            }, {
-                // Images
-                test: /images.*\.(jpe?g|png|svg|gif)$/,
-                loaders: [
-                    'file?name=./images/[name].[hash].[ext]',
-                    'image-webpack?optimizationLevel=7&interlaced=false'
-                ],
-            }, {
-                // static files
-                test: /data/,
-                loaders: [
-                    'file?name=data/[name].[ext]'
-                ]
-            }, {
-                // Fonts
-                test: /\.(eot|ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file?name=./fonts/[name].[ext]'
-            }, {
-                // Handlebars
-                test: /\.handlebars?$/,
-                loader: 'handlebars-loader'
-            }, {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    resolve: {
-        root: path.resolve(__dirname, './src'),
-        // Add `.ts` and `.tsx` as a resolvable extension.
-        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
-        modulesDirectories: ['node_modules']
-    },
-    stats: {
-        children: false
-    },
-    plugins: [
-        new ExtractTextPlugin('./styles.css'),
-        new HtmlWebpackPlugin({
-            title: 'Faire Guide',
-            template: 'html!./src/index.html',
-        }),
-        new webpack.ProvidePlugin({
-            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-        }),
-    ],
-    devServer: {
-        stats: {
-            assets: false,
-            colors: true,
-            version: false,
-            hash: false,
-            timings: false,
-            chunks: false,
-            chunkModules: false,
-            children: false
-        }
-    },
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "docs"),
+    filename: "scripts/main.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        "sass-loader"
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HandlebarsPlugin({
+      entry: path.join(process.cwd(), "app", "src", "*.hbs"),
+      output: path.join(process.cwd(), "build", "[name].html"),
+      data: path.join(__dirname, "app/data/project.json"),
+      partials: [
+        path.join(process.cwd(), "app", "src", "components", "*", "*.hbs"),
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: "src/data/*",
+        to: "docs/data"
+      }, {
+        from: "src/fonts/*",
+        to: "docs/fonts"
+      }, {
+        from: "src/images/*",
+        to: "docs/images"
+      }]
+    })
+  ],
 };
